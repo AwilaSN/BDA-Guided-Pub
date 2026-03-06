b46557c4df454d8bf41b39f89c6b293ce1cb9788
BDA <Awilenya@gmail.com>
2026-03-06 17:06:45 -0500

    FirstCommit

---
+++ Main.py
@@ -0,0 +1,10 @@
+# This is my first tutorial at learning python
+# Anything in front of a = is a variable. / = creates a variable
+# Anything after = is a string if in "" or ''
+# Integer is a number/value after =
+# A integer or string can be turned into opposite with str(variable)/int(variable)
+name_first = "Hello"
+name_last = " Eva Kodi"
+age = 6
+Age = "16"
+print(name_first +  name_last + str(age))
